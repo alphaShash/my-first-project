bf9848a460e4614ca82342bb93cb90b96d41712a
alphaShash <shashidharmaregowd@gmail.com>
2022-09-15 17:21:39 +0200

    First App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  //template: `<p> {{myObject.color}}</p>`,
+  //template: `<ul>
+  //<li *ngFor = "let samplearray of myArray">{{samplearray}}</li>
+  //</ul>`,
+  template: `<ul>
+  <li *ngIf = "myArray; else idTemplate">Type the array is present</li>
+  </ul>
+  <ng-template #idTemplate>Array is Empty</ng-template>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  //title = 'my-first-project';
+  myObject = {
+    color:'blue',
+    material : 'soft'
+  };
+  myArray = false
 }
